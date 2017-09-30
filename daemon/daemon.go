6bdb790d6ed765459dcc482a21be706e281f7398
Anton Novojilov <andy@essentialkaos.com>
2017-09-30 18:37:57 +0200

    Improvements

---
+++ daemon/daemon.go
@@ -0,0 +1,131 @@
+package daemon
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2017 ESSENTIAL KAOS                         //
+//        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"os"
+
+	"pkg.re/essentialkaos/ek.v9/fmtc"
+	"pkg.re/essentialkaos/ek.v9/options"
+	"pkg.re/essentialkaos/ek.v9/usage"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+const (
+	APP  = "Bastion"
+	VER  = "1.0.0"
+	DESC = "Utility for temporary access limitation to server"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Daemon info
+const (
+	MAIN_DURATION   = "main:duration"
+	MAIN_URL        = "main:url"
+	SERVER_IP       = "server:ip"
+	SERVER_PORT     = "server:port"
+	LOG_DIR         = "log:dir"
+	LOG_FILE        = "log:file"
+	LOG_PERMS       = "log:perms"
+	LOG_LEVEL       = "log:level"
+	SCRIPT_BEFORE   = "script:before"
+	SCRIPT_START    = "script:start"
+	SCRIPT_END      = "script:end"
+	SCRIPT_COMPLETE = "script:complete"
+)
+
+// Options
+const (
+	OPT_CONFIG   = "c:config"
+	OPT_NO_COLOR = "nc:no-color"
+	OPT_HELP     = "h:help"
+	OPT_VER      = "v:version"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Options map
+var optMap = options.Map{
+	OPT_CONFIG:   {Value: "/etc/bastion.knf"},
+	OPT_NO_COLOR: {Type: options.BOOL},
+	OPT_HELP:     {Type: options.BOOL, Alias: "u:usage"},
+	OPT_VER:      {Type: options.BOOL, Alias: "ver"},
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Main function
+func Init() {
+	_, errs := options.Parse(optMap)
+
+	if len(errs) != 0 {
+		for _, err := range errs {
+			printError(err.Error())
+		}
+
+		os.Exit(1)
+	}
+
+	if options.GetB(OPT_NO_COLOR) {
+		fmtc.DisableColors = true
+	}
+
+	if options.GetB(OPT_VER) {
+		showAbout()
+		return
+	}
+
+	if options.GetB(OPT_HELP) {
+		showUsage()
+		return
+	}
+}
+
+// printError prints error message to console
+func printError(f string, a ...interface{}) {
+	fmtc.Fprintf(os.Stderr, "{r}"+f+"{!}\n", a...)
+}
+
+// printError prints warning message to console
+func printWarn(f string, a ...interface{}) {
+	fmtc.Fprintf(os.Stderr, "{y}"+f+"{!}\n", a...)
+}
+
+// printErrorAndExit print error mesage and exit with exit code 1
+func printErrorAndExit(f string, a ...interface{}) {
+	printError(f, a...)
+	os.Exit(1)
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func showUsage() {
+	info := usage.NewInfo()
+
+	info.AddOption(OPT_CONFIG, "Path to config file", "file")
+	info.AddOption(OPT_NO_COLOR, "Disable colors in output")
+	info.AddOption(OPT_HELP, "Show this help message")
+	info.AddOption(OPT_VER, "Show version")
+
+	info.Render()
+}
+
+func showAbout() {
+	about := &usage.About{
+		App:     APP,
+		Version: VER,
+		Desc:    DESC,
+		Year:    2006,
+		Owner:   "ESSENTIAL KAOS",
+		License: "Essential Kaos Open Source License <https://essentialkaos.com/ekol>",
+	}
+
+	about.Render()
+}
